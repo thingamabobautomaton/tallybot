1e144fddf4fa68ad76596b63826ce7eb0ae427c6
swe437gmu <swe437.gmu@gmail.com>
2015-07-18 19:58:21 -0400

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var Slack = require('node-slack');
+var slack = new Slack('https://hooks.slack.com/services/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');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* POST to yesman */
+router.post('/yesman',function(req,res) {
+  var reply = slack.respond(req.body,function(hook) {
+
+    return {
+      text: 'Good point, ' + hook.user_name,
+      username: 'Bot'
+    };
+
+  });
+
+  res.json(reply);
+
+});
+
+module.exports = router;
